6f107b1c5f83678f73fc2c205b74d0d1c74d7f93
Klackky <natalia.volchatova@gmail.com>
2018-01-19 17:07:03 -0500

    webpack и его невероятные костыли

--- src/app.js
+++ src/app.js
@@ -2,3 +2,4 @@ import './js/rainbows.js';
 import './js/unicorns.js';
 import './js/fontawesome-all.js';
 import './scss/base.scss';
+import './scss/header.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,31 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
+var resolveUrlLoader = require("resolve-url-loader");
 module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath:'./dist'
   },
   module: {
   rules: [
     {
-      test: /\.scss$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: ['css-loader', 'sass-loader']
-      })
-    }
-  ]
+         test   : /\.css$/,
+         loaders: ['style-loader', 'css-loader?url=false', 'resolve-url-loader']
+       }, {
+         test   : /\.scss$/,
+         loaders: ['style-loader', 'css-loader?url=false', 'resolve-url-loader', 'sass-loader']
+       },
+       {
+        test: /\.(png|jpeg|ttf|...)$/,
+        loader: 'url-loader',
+        query: {
+          limit:10000,
+          name: '[name].[ext]',
+        }
+      }
+      ]
 },
   plugins: [
    new ExtractTextPlugin('style.css')
