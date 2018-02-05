7fb5d962ea0d90538c91d60b1b544a2c5bbc878e
Klackky <natalia.volchatova@gmail.com>
2018-02-05 16:53:59 -0500

    new webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,56 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-var resolveUrlLoader = require("resolve-url-loader");
+const resolveUrlLoader = require("resolve-url-loader");
+const extractSass = new ExtractTextPlugin({
+    filename: '[name].css',
+});
+const isDevelopment = !process.env.production;
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath:'./dist'
-  },
-  module: {
-  rules: [
-    {
-         test   : /\.css$/,
-         loaders: ['style-loader', 'css-loader?url=false', 'resolve-url-loader'],
-         exclude: /node_modules/
-       }, {
-         test   : /\.scss$/,
-         loaders: ['style-loader', 'css-loader?url=false', 'resolve-url-loader', 'sass-loader'],
-         exclude: /node_modules/
-       },
-       {
-        test: /\.(png|jpeg|ttf|...)$/,
-        loader: 'url-loader',
-        exclude: /node_modules/,
-        query: {
-          limit:10000,
-          name: '[name].[ext]',
-        }
-      }
-      ]
-},
-  plugins: [
-   new ExtractTextPlugin('style.css'),
-   new UglifyJsPlugin()
-  ]
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: './dist/'
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader', 'resolve-url-loader'),
+                exclude: /node_modules/
+            }, {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader?sourceMap']
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: 'images/[name][hash].[ext]'
+                    }
+                }, {
+                    loader: 'image-webpack-loader',
+                    options: {
+                        mozjpeg: {
+                            progressive: true,
+                            quality: 70
+                        }
+                    }
+                }, ],
+            }, {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'fonts/[name][hash].[ext]'
+                    }
+                },
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css'),
+        new UglifyJsPlugin()
+    ]
 };
